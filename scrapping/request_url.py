05c1b05a8963b5c264bfc45fdbeacfe6b7049115
Francisco <fvcneto.master98@gmail.com>
2018-12-25 16:44:03 -0200

    implement raise exceptions

--- scrapping/request_url.py
+++ scrapping/request_url.py
@@ -1,26 +1,32 @@
 import re
 import requests
 
+from .url_exception import URLException
+
 class Url:
     def __init__(self, link):
         self.link = link
         self._url = self._downloadable_url()
 
     def _downloadable_url(self):
+        # change 'www' to 'mbasic' in the url
         try:
-            # change 'www' to 'mbasic' in the url
             temp_url = re.sub(r'\bwww\b', 'mbasic', self.link)
             html = requests.get(temp_url).text
+        except:
+            raise URLException("error: can't substitute 'www' for 'mbasic'")
 
-            # regex to get downloadable url
+        # regex to get downloadable url
+        try:
             down_url = re.findall('<a href="/video_redirect/(.*?)"', html)
+        except:
+            raise URLException("error: can't regex to get downloadable url")
 
-            # scrapping the down_url to get the downloadble url
+        # scrapping the down_url to get the downloadble url
+        try:
             return self._uncrypt_url(requests.get('https://mbasic.facebook.com/video_redirect/' + down_url[0]).url)
-        except Exception as e:
-            print(e)
-        else:
-            return None
+        except:
+            raise URLException("error: can't uncrypt url")
 
     def _uncrypt_url(self, url):
         h1 = re.findall('https%253A%252F%252F(.*?)%', url)[0]
