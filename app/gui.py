7663f2f40ef0ae82001bfc8d38b69529bdb48f98
Francisco <fvcneto.master98@gmail.com>
2018-12-25 13:31:00 -0200

    browse and open file buttons implemetation

--- app/gui.py
+++ app/gui.py
@@ -1,13 +1,18 @@
-import tkinter as tk
-import settings
-from scrapping.request_url import Url
 import requests
 import urllib.request
+import os
+import tkinter as tk
+
+import settings
+
+from scrapping.request_url import Url
+from tkinter.filedialog import askdirectory
 
 class Gui(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         self.master = master
+        self._file_path = tk.StringVar()
         self.config()
         self.pack()
         self.create_widgets()
@@ -26,11 +31,9 @@ class Gui(tk.Frame):
         self.path_frame = tk.Frame(self)
         self.path_frame.grid(row=0, column=0, padx=settings.FRAME_PADDING_X, pady=settings.FRAME_PADDING_Y)
 
-        self.file_path = tk.StringVar()
-
         tk.Label(self.path_frame, text="Rota do arquivo").grid(row=0, column=0)
-        self.path_text = tk.Entry(self.path_frame, textvariable=self.file_path, width=settings.ENTRY_WIDTH)
-        self.file_path.set(settings.DOWNLOAD_PATH)
+        self.path_text = tk.Entry(self.path_frame, textvariable=self._file_path, width=settings.ENTRY_WIDTH)
+        self.set_file_path(settings.DOWNLOAD_PATH) # changes the text of the path_text entry
         self.path_text.grid(row=1, column=0, padx=10)
 
         self.path_btn = tk.Button(self.path_frame, text="Browse", width=settings.BUTTON_WIDTH, command=self.browse_click)
@@ -51,9 +54,6 @@ class Gui(tk.Frame):
         self.file_name_entry = tk.Entry(self.download_frame, width=settings.ENTRY_WIDTH)
         self.file_name_entry.grid(row=3, column=0, padx=10)
 
-        self.enter_name_btn = tk.Button(self.download_frame, text="Enter", width=settings.BUTTON_WIDTH, command=self.enter_name_click, state='disabled')
-        self.enter_name_btn.grid(row=3, column=1)
-
         # log frame
         self.log_frame = tk.Frame(self)
         self.log_frame.grid(row=2, column=0,padx=settings.FRAME_PADDING_X, pady=settings.FRAME_PADDING_Y)
@@ -67,7 +67,7 @@ class Gui(tk.Frame):
 
         self.cancel_btn = tk.Button(self.control_frame, text="Cancelar", width=settings.BUTTON_WIDTH, command=self.cancel_click)
         self.cancel_btn.grid(row=0, column=0, padx=5, pady=5)
-        self.open_folder_btn = tk.Button(self.control_frame, text="Abrir Pasta",width=settings.BUTTON_WIDTH, command=self.open_file_click, state='disabled')
+        self.open_folder_btn = tk.Button(self.control_frame, text="Abrir Pasta",width=settings.BUTTON_WIDTH, command=self.open_folder_click)
         self.open_folder_btn.grid(row=1, column=0, padx=5, pady=5)
 
     def download_click(self):
@@ -93,15 +93,20 @@ class Gui(tk.Frame):
         self.master.destroy()
 
     def browse_click(self):
-        pass
+        self.master.withdraw() # hide the root
+        dirname = askdirectory(initialdir=os.getcwd(),title="Selecione a pasta onde será salvo o vídeo")
+        if len(dirname) > 0:
+            self.set_file_path(dirname)
+        self.master.deiconify() # show the root again
 
-    def open_file_click(self):
-        pass
-
-    def enter_name_click(self):
-        pass
+    def open_folder_click(self):
+        os.startfile(self.path_text.get())
 
     def set_log_msg(self, log):
         msg = settings.LOG_MESSAGES[log]
         self.log_label['text'] = msg[0]
         self.log_label['foreground'] = msg[1]
+
+    def set_file_path(self, new_path):
+        self._file_path.set(new_path)
+    
