6051a5259b862248d9e10aa37aaabacb50482435
Francisco Neto <fvcneto.master98@gmail.com>
2019-01-17 11:59:41 -0300

    reorganize folders

---
+++ fb_vd/settings.py
@@ -0,0 +1,23 @@
+import os
+
+## settings ##
+
+## path
+# download path
+DOWNLOAD_PATH = os.getcwd()+"/videos/"
+
+## widgets
+BUTTON_WIDTH = 10
+ENTRY_WIDTH = 50
+
+# frame
+FRAME_PADDING_X = 10
+FRAME_PADDING_Y = 10
+
+# log messages
+LOG_MESSAGES = {
+    "error": ("Erro: Download não pode ser concluido!", "red"),
+    "processing": ("Download em progresso...", "blue"),
+    "completed": ("Download completo!", "green")
+}
+
